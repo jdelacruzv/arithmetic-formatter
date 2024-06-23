e6ebfac996a1298513120b1f2c5326c52bc46f46
Jose De La Cruz <jldlcv@gmail.com>
2024-06-23 13:09:19 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Program that organizes arithmetic problems (addition and subtraction) vertically to make them easier to solve.
---
+++ main.py
@@ -0,0 +1,7 @@
+def arithmetic_arranger(problems, show_answers=False):
+
+    return problems
+
+
+resul = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+print(resul)
