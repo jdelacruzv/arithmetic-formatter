71f7c5bcc80263f6a2be5a4b241b7c6ce9f49952
Jose De La Cruz <jldlcv@gmail.com>
2024-07-14 12:20:25 +0200

    Added function to validate string that has only digits

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+# Validate that the list is only digits
+def only_digits(str):
+    digits = ''
+    for x in str:
+        for y in x:
+            if not y.isdigit():
+                digits = digits + y.strip()
+    # Returns string without digits
+    return digits
+
+
 def arithmetic_arranger(problems, show_answers=False):
+    result = only_digits(problems)
+    print(result)
 
-    return problems
+    if len(problems) > 5:
+        print('Error: Too many problems')
+
+    if ('*' in result) or ('/' in result):  # aqui esta mal
+        print('Error: Operator must be \'+\' or \'-\'')
+
+    for res in result:
+        if res.isalpha():
+            print('Error: Numbers must only contain digits')
 
 
-resul = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-print(resul)
+# arithmetic_arranger(['32 + 698', '3801 - 2', '45 + 43', '123 + 49', '1', '2'])
+# arithmetic_arranger(['32 + 698', '3801 / 2', '45 + 43', '123 * 49'])
+arithmetic_arranger(['e2 + 698', '3801 - 2', '45 + 43', '123 + 49'])
