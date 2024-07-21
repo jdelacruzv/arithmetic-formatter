46741918243cdedb26fb98055b77822fb6a4ba11
Jose De La Cruz <jldlcv@gmail.com>
2024-07-21 21:09:21 +0200

    The only_letters function was modified

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
-# Validate that the list is only digits
-def only_digits(str):
-    digits = ''
-    for x in str:
+# Returns string containing at least one letter
+def only_letters(lst):
+    letter = ''
+    for x in lst:
         for y in x:
             if not y.isdigit():
-                digits = digits + y.strip()
-    # Returns string without digits
-    return digits
+                letter = letter + y.strip()
+    return letter
+
+
+def z():
+    pass
+
+# Returns a boolean
+def count_digits(lst):
+    lst_to_str = ' '.join(lst)
+    str_to_lst = lst_to_str.split()
+    print('Original:', lst)
+    print('Cadena:', lst_to_str)
+    print('Lista:', str_to_lst)
+    
+    temp = False
+    for x in str_to_lst:
+        if len(x) > 4:
+            temp = True
+    return temp
 
 
 def arithmetic_arranger(problems, show_answers=False):
-    result = only_digits(problems)
-    print(result)
-
     if len(problems) > 5:
         print('Error: Too many problems')
 
-    if ('*' in result) or ('/' in result):  # aqui esta mal
+    letters = only_letters(problems)
+    print('String:', letters)
+
+    if ('*' in letters) or ('/' in letters):  # aqui esta mal
         print('Error: Operator must be \'+\' or \'-\'')
 
-    for res in result:
-        if res.isalpha():
+    for letter in letters:
+        if letter.isalpha():
             print('Error: Numbers must only contain digits')
 
+    a = count_digits(problems)
+    if a:
+        print('Error: Numbers cannot be more than four digits')
+    else:
+        print('OK')
 
 # arithmetic_arranger(['32 + 698', '3801 - 2', '45 + 43', '123 + 49', '1', '2'])
 # arithmetic_arranger(['32 + 698', '3801 / 2', '45 + 43', '123 * 49'])
-arithmetic_arranger(['e2 + 698', '3801 - 2', '45 + 43', '123 + 49'])
+# arithmetic_arranger(['e2 + 698', '3801 - 2', '45 + 43', '123 + 49'])
+arithmetic_arranger(['32 + 698', '38011 - 2', '45 + 43', '123 + 49'])
