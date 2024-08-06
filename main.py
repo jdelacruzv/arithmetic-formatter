0d7d88f4c1805dc5418d2e252fa6694cdc03515f
Jose De La Cruz <jldlcv@gmail.com>
2024-08-06 22:33:42 +0200

    Added arithmetic_arranger() function

--- main.py
+++ main.py
@@ -33,6 +33,55 @@ def number_less_four_digits(operations):
     return is_true
 
 
+def arithmetic_arranger(problems, show_answers=False):
+    data = [[], [], [], []]
+    final_data = []
+    results = convert_list(problems)
+    digits = remove_characters(results)
+
+    if len(results) > 15:
+        return print('Error: Too many problems.')
+
+    elif ('*' in results) or ('/' in results):
+        return print("Error: Operator must be '+' or '-'.")
+
+    elif only_digits(digits):
+        return print('Error: Numbers must only contain digits.')
+
+    elif number_less_four_digits(digits):
+        return print('Error: Numbers cannot be more than four digits.')
+
+    else:
+        for problem in problems:
+            items = problem.split(' ')
+
+            if len(items[2]) >= len(items[0]):
+                second = f'{items[1]} {items[2]}'
+            else:
+                second = f'{items[1]}{' ' * (len(items[0]))}{items[2]}'
+
+            data[0].append(' ' * (len(second) - len(items[0])) + items[0])
+            data[1].append(second)
+            data[2].append('-' * len(second))
+
+            # Evaluates a mathematical expression passed as a string
+            sum_items = eval(items[0] + items[1] + items[2])
+            data[3].append(
+                ' ' * (len(second) - len(str(sum_items))) + str(sum_items))
+
+        if show_answers:
+            final_data.append('    '.join(data[0]))
+            final_data.append('    '.join(data[1]))
+            final_data.append('    '.join(data[2]))
+            final_data.append('    '.join(data[3]))
+        else:
+            final_data.append('    '.join(data[0]))
+            final_data.append('    '.join(data[1]))
+            final_data.append('    '.join(data[2]))
+
+        return print('\n'.join(final_data))
+
+
 # arithmetic_arranger(['32 + 698', '3801 - 2', '45 + 43', '123 + 49', '1', '2'])
 # arithmetic_arranger(['32 + 698', '3801 / 2', '45 + 43', '123 * 49'])
 # arithmetic_arranger(['e2 + 698', '3801 - 2', '45 + 43', '123 + 49'])
